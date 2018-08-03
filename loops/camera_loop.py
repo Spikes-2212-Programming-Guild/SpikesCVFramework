6d2e8e2aaf1c287f525e56124cbd0e378f832a8f
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2018-08-04 01:28:36 +0300

    added capturing parameter to loops

--- loops/camera_loop.py
+++ loops/camera_loop.py
@@ -1,8 +1,8 @@
 import constants
 
 
-def camera_loop(locked_image, camera_manager, settings):
-    while True:
+def camera_loop(locked_image, camera_manager, settings, capturing):
+    while capturing():
         if settings.is_updated():
             camera_manager.set_exposure(settings.get().get(constants.exposure_settings_key, ""))
             camera_manager.set_camera(settings.get().get(constants.camera_id_settings_key, ""))
--- loops/pipeline_loop.py
+++ loops/pipeline_loop.py
@@ -1,8 +1,8 @@
 import constants
 
 
-def pipeline_loop(locked_image, pipeline_manager, settings, output_consumer):
-    while True:
+def pipeline_loop(locked_image, pipeline_manager, settings, output_consumer, capturing):
+    while capturing():
         if settings.is_updated():
             pipeline_manager.set_pipeline(settings.get().get(constants.pipeline_name_settings_key, ''))
         img = locked_image.get()
