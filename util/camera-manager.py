5742ae0cdcc9fda46e5db67c44ff013f05dbf1fa
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2018-08-04 01:14:13 +0300

    made camera-manager safer

--- util/camera-manager.py
+++ util/camera-manager.py
@@ -10,7 +10,8 @@ class CameraManager:
             self.current_id = cameras.keys()[0]
 
     def set_camera(self, camera_id):
-        self.current_id = camera_id
+        if self.current_id is not camera_id and self.current_id in self.cameras.keys():
+            self.current_id = camera_id
 
     def set_exposure(self, exposure):
         call(f"v412-ctl --device=/dev/video{self.current_id} -c exposure_auto=1 -c exposure_absolute={exposure}")
