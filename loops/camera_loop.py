a8ca05b4adf6dd93ac0990e41b9571bbde6b3eb5
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2018-08-04 01:15:05 +0300

    added camera loop

---
+++ loops/camera_loop.py
@@ -0,0 +1,12 @@
+import constants
+
+
+def camera_loop(locked_image, camera_manager, settings):
+    while True:
+        if settings.is_updated():
+            camera_manager.set_exposure(settings.get().get(constants.exposure_settings_key, ""))
+            camera_manager.set_camera(settings.get().get(constants.camera_id_settings_key, ""))
+
+        img = camera_manager.get_image()
+        if img is not None:
+            locked_image.set(img)
