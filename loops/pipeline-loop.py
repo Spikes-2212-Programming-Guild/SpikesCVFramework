8392a9eecd03d81e9d5377dd5e742a5469593e24
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2018-08-04 01:03:37 +0300

    added pipeline-loop function

---
+++ loops/pipeline-loop.py
@@ -0,0 +1,10 @@
+import constants
+
+
+def pipeline_loop(locked_image, pipeline_manager, settings, output_consumer):
+    while True:
+        if settings.is_updated():
+            pipeline_manager.set_pipeline(settings.get().get(constants.pipeline_name_settings_key, ''))
+        img = locked_image.get()
+        output = pipeline_manager.process(img)
+        output_consumer(output)
