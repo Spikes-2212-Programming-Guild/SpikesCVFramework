5b04781011e4e2a4e7e5ece6d4f28ea86c8ea0d7
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2018-08-05 00:35:56 +0300

    made camera_loop release camera_manager

--- loops/camera_loop.py
+++ loops/camera_loop.py
@@ -10,3 +10,4 @@ def camera_loop(locked_image, camera_manager, settings, running):
         img = camera_manager.get_image()
         if img is not None:
             locked_image.set(img)
+    camera_manager.release()
