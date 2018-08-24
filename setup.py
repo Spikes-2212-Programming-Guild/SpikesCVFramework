45326dfa64a5ae11c6381622869380bba88cc222
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2018-08-24 11:23:54 +0300

    updated version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="scvf",
-    version="0.0.7",
+    version="0.0.8",
     author="Spikes#2212",
     author_email="spikesno2212@gmail.com",
     description="a framework that wraps opencv for our needs in FRC",
