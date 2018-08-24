e4236010e3f5b50c44a7edbdffea2e41b8bf9fa1
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2018-08-24 11:23:39 +0300

    added None safety to pipeline_loop

--- scvf/loops/pipeline_loop.py
+++ scvf/loops/pipeline_loop.py
@@ -6,5 +6,6 @@ def pipeline_loop(locked_image, pipeline_manager, settings, output_consumer, run
         if settings.is_updated():
             pipeline_manager.set_pipeline(settings.get().get(constants.pipeline_name_settings_key, ''))
         img = locked_image.get()
-        pipeline_manager.process(img)
-        output_consumer(pipeline_manager.get_contours())
+        if img is not None:
+            pipeline_manager.process(img)
+            output_consumer(pipeline_manager.get_contours())
