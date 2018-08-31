aa827c09fed4359bd37d19bcb3c76f260963da9b
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2018-08-31 16:32:18 +0300

    removed thread safety from settings

--- scvf/util/settings.py
+++ scvf/util/settings.py
@@ -1,24 +1,17 @@
-from threading import Lock
-
-
 class Settings:
     def __init__(self):
         self.__values = dict()
         self.__isUpdated = False
-        self.__lock = Lock()
 
     def update(self, **values):
-        with self.__lock:
-            for key in values.keys():
-                if self.__values.get(key, "") != values.get(key, ""):
-                    self.__isUpdated = True
-                    self.__values[key] = values[key]
+        for key in values.keys():
+            if self.__values.get(key, "") != values.get(key, ""):
+                self.__isUpdated = True
+                self.__values[key] = values[key]
 
     def get(self):
-        with self.__lock:
-            self.__isUpdated = False
-            return self.__values.copy()
+        self.__isUpdated = False
+        return self.__values.copy()
 
     def is_updated(self):
-        with self.__lock:
-            return self.__isUpdated
+        return self.__isUpdated
