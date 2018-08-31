bb6f32416aceab7c0a2300ca6d0560ca72e7b09f
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2018-08-31 16:30:25 +0300

    fixed call

--- scvf/loops/__init__.py
+++ scvf/loops/__init__.py
@@ -1,3 +1,3 @@
 from scvf.loops.camera_loop import camera_loop
 from scvf.loops.pipeline_loop import pipeline_loop
-from scvf.loops.settings_loop import settings_loop
+from scvf.loops.settings_callback import callback as settings_callback
