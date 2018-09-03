d1e9f92a9f4e5331bc102371f01713c3a90824b2
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2018-09-03 09:29:31 +0300

    added lock to safe_image

--- scvf/util/safe_image.py
+++ scvf/util/safe_image.py
@@ -1,13 +1,18 @@
+from threading import Lock
+
 
 class SafeImage:
     def __init__(self):
+        self.lock = Lock()
         self.old_img = None
         self.new_img = None
 
     def set(self, img):
-        self.new_img = img
+        while self.lock:
+            self.new_img = img
 
     def get(self):
-        b = self.old_img
-        self.old_img = self.new_img
-        return b
+        with self.lock:
+            b = self.old_img
+            self.old_img = self.new_img
+            return b
