87d19e510a52f4a50f79f4d34e0014da987739bb
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2018-09-22 13:07:20 +0300

    made constants a dictionary

--- scvf/loops/camera_loop.py
+++ scvf/loops/camera_loop.py
@@ -1,11 +1,11 @@
-from scvf import constants
+from scvf.constants import settings_keys
 
 
 def camera_loop(safe_image, camera_manager, settings, running):
     while running():
         if settings.is_updated():
-            camera_manager.set_exposure(settings.get(constants.exposure_settings_key, ""))
-            camera_manager.set_camera(settings.get(constants.camera_id_settings_key, ""))
+            camera_manager.set_exposure(settings.get(settings_keys["exposure"], ""))
+            camera_manager.set_camera(settings.get(settings_keys["camera_id"], ""))
 
         img = camera_manager.get_image()
         if img is not None:
--- scvf/loops/pipeline_loop.py
+++ scvf/loops/pipeline_loop.py
@@ -1,10 +1,10 @@
-from scvf import constants
+from scvf.constants import settings_keys
 
 
 def pipeline_loop(safe_image, pipeline_manager, settings, output_consumer, running):
     while running():
         if settings.is_updated():
-            pipeline_manager.set_pipeline(settings.get(constants.pipeline_name_settings_key, ''))
+            pipeline_manager.set_pipeline(settings.get(settings_keys["pipeline_name"], ''))
         img = safe_image.get()
         if img is not None:
             pipeline_manager.process(img)
