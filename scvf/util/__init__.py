c437c7565ba275aa65fcf300ae2d5f5253216c34
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2018-09-22 13:52:26 +0300

    changed safe_image to image_container

--- scvf/util/__init__.py
+++ scvf/util/__init__.py
@@ -1,4 +1,4 @@
 from scvf.util.camera_manager import CameraManager
-from scvf.util.image_container import SafeImage
+from scvf.util.image_container import ImageContainer
 from scvf.util.pipeline_manager import PipelineManager
 from scvf.util.settings import Settings
