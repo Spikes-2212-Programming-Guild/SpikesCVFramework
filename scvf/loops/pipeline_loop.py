c85a85b13519d37049a13f6e54dcdcb58785eadc
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-01-25 11:43:06 +0200

    documented pipeline_loop

--- scvf/loops/pipeline_loop.py
+++ scvf/loops/pipeline_loop.py
@@ -2,6 +2,21 @@ from scvf.constants import settings_keys
 
 
 def pipeline_loop(image_container, pipeline_manager, settings, output_consumer, running):
+    """
+    This function creates a loop that manages all the pipelines.
+    it is responsible for processing an image and sending the output to the output_consumer,
+    as well as applying new settings to the pipelines if they arrive.
+    :param image_container:
+        a container that holds the latest image
+    :param pipeline_manager:
+        a ``PipelineManager`` instance that holds all the pipelines
+    :param settings:
+        the ``Settings`` instance for this loop
+    :param output_consumer:
+        the function that should receive the processed output of the pipeline
+    :param running:
+        a lambda that tells the pipeline_loop whether it should continue to run
+    """
     while running():
         if settings.is_updated():
             pipeline_manager.set_pipeline(settings.get(settings_keys["pipeline_name"], ''))
