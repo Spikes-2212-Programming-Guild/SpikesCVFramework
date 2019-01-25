9497f1767629b5f6d4a70ca828e64dd31c0020c0
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-01-25 11:39:18 +0200

    documented settings_callback

--- scvf/loops/settings_callback.py
+++ scvf/loops/settings_callback.py
@@ -1,6 +1,17 @@
 
 
 def callback(camera_settings, pipeline_settings):
+    """
+        this function generates a callback should be called each time a new settings arrives.
+        the ``settings_supplier`` is responsible for receiving new settings and calling this function when
+        a new settings arrives. in turn, the callback updates the given setting sets with the setting.
+    :param camera_settings:
+        the settings for the ``camera_loop``
+    :param pipeline_settings:
+        the settings for the ``pipeline_loop``
+    :return:
+        a callback that updates the given settings
+    """
     def c(key, value):
         camera_settings.update(**{key: value})
         pipeline_settings.update(**{key: value})
